3816cafb003284676ed9afe0ed197bda73bb2cca
Dustin Boling <dustin@dustinboling.com>
2019-02-11 09:32:52 -0800

    removes ignored files

--- vendor/dmitrybubyakin/nova-medialibrary-field/resources/js/field.js
+++
@@ -1,5 +0,0 @@
-Nova.booting((Vue, router) => {
-    Vue.component('index-nova-medialibrary-field', require('./components/IndexField'))
-    Vue.component('detail-nova-medialibrary-field', require('./components/DetailField'))
-    Vue.component('form-nova-medialibrary-field', require('./components/FormField'))
-})
