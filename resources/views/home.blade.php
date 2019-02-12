73eab71ee7a52514bbdfe93484ef6feaf43ba173
Dustin Boling <dustin@dustinboling.com>
2019-02-12 02:02:47 -0800

    reduces size of jumbotron heading

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,7 @@
     <div class="row">
         <div class="col">
             <div class="jumbotron bg-erg-gradient text-white">
-                <h1 class="display-4">Hello, {{ auth()->user()->name }}!</h1>
+                <h1 class="display-5">Hello, {{ auth()->user()->name }}!</h1>
                 <p class="lead">Welcome to our company wide directory, file storage and Intranet. We have company logos, your business card photos, training videos, training audio files, an e-book, and checklists galore on here.</p>
                 <hr class="my-4">
                 <p>Feel free to browse around and we hope you will find everything you need in a nice, orderly format.</p>
