9c932af51583c94b5a7d330b318ef31c47902625
Dustin Boling <dustin@dustinboling.com>
2019-03-06 23:44:21 -0800

    Uses Bootstrap for responsive video

--- resources/views/notes/partials/youtube.blade.php
+++ resources/views/notes/partials/youtube.blade.php
@@ -1,3 +1,3 @@
-<div class="videoWrapper">
-    <iframe id="ytplayer" type="text/html" width="498" height="280.125" src="https://www.youtube.com/embed/{{ $youtubeUrl }}?modestbranding=1&iv_load_policy=3&rel=0" frameborder="0" allowfullscreen></iframe>
+<div class="embed-responsive embed-responsive-16by9">
+    <iframe class="embed-responsive-item" id="ytplayer" type="text/html" width="498" height="280.125" src="https://www.youtube.com/embed/{{ $youtubeUrl }}?modestbranding=1&iv_load_policy=3&rel=0" allowfullscreen></iframe>
 </div>
