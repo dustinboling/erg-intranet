c1fecb51adf6a7e90cfd119bc84554bd4ccaaa0b
Dustin Boling <dustin@dustinboling.com>
2019-07-08 21:30:45 -0700

    fixes wrong url to Real Geeks

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -55,7 +55,7 @@
                         <!-- Authentication Links -->
                         @guest
                             <li class="nav-item">
-                                <a href="https://login.realgeeks.com/auth/login/3" target="_blank" class="nav-link">Real Geeks</a>
+                                <a href="https://leads.realgeeks.com/leads" target="_blank" class="nav-link">Real Geeks</a>
                             </li>
                             <li class="nav-item">
                                 <a href="https://apps.rackspace.com/index.php" target="_blank" class="nav-link">Webmail</a>
